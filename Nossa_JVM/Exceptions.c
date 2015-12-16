8a2731d74b52139e4cfc00ea8ad56abfa1b4b500
Luiz Henrique Campos Barboza <luizhcb0@gmail.com>
2015-12-16 04:04:10 -0200

    Error

--- Nossa_JVM/Exceptions.c
+++ Nossa_JVM/Exceptions.c
@@ -20,31 +20,29 @@ void error(int errorCode){
             printf("\n");
             //sleep(2);
             exit(1);
-        case NOTACLASS_ERR 2                               :
-        case INCORRECTVERSION_ERR 1:
-        case E_SUCCESS  0:
-        case E_INVALID_NUM_ARGS  -1:
-        case E_ARGV1_FILE_NOT_EXISTENT  -2 :
-        case E_CAFEBABE  -3:
-        case E_VERSION  -4:
-        case W_NAOLIDOINTEIRO  -5 :
-        case E_OPCAO_NAO_EXISTENTE  -6:
-        case E_DOLAR_NOT_SUPPORTED  -7 :
-        case E_EMPTY_FRAME_STACK  -8:
-        case E_STACK_OVERFLOW  -9:
-        case E_NO_MAIN_FOUND  -10:
-        case E_EMPTY_STACK  -11:
-        case E_NOTSUPPORTED_VERSION  -12:
-        case E_NOTVALID_CP_INDEX  -13:
-        case E_NEG_ARR_SIZE  -14:
-        case E_NULL_POINTER  -15:
-        case E_ARR_NOT_FOUND  -16 :
-        case E_NOT_VALID_MARRAY_INFO  -17            :
-        case E_NOT_SUPPORTED_ARRAY_TYPE  -18         :
-        case E_DISTINCT_MARRAY_DIMENSIONS  -19       :
-        case E_NOT_SUPPORTED_INSTRUCTION  -20        :
-        case E_BAD_INPUT  -21                        :
-        case E_NEGATIVE_ARRAY_SIZE  -22              :
+        case E_SUCCESS:
+        case E_INVALID_NUM_ARGS:
+        case E_ARGV1_FILE_NOT_EXISTENT:
+        case E_CAFEBABE:
+        case E_VERSION:
+        case W_NAOLIDOINTEIRO:
+        case E_OPCAO_NAO_EXISTENTE:
+        case E_DOLAR_NOT_SUPPORTED:
+        case E_EMPTY_FRAME_STACK:
+        case E_STACK_OVERFLOW:
+        case E_NO_MAIN_FOUND:
+        case E_EMPTY_STACK:
+        case E_NOTSUPPORTED_VERSION:
+        case E_NOTVALID_CP_INDEX:
+        case E_NEG_ARR_SIZE:
+        case E_NULL_POINTER:
+        case E_ARR_NOT_FOUND:
+        case E_NOT_VALID_MARRAY_INFO            :
+        case E_NOT_SUPPORTED_ARRAY_TYPE          :
+        case E_DISTINCT_MARRAY_DIMENSIONS        :
+        case E_NOT_SUPPORTED_INSTRUCTION         :
+        case E_BAD_INPUT                        :
+        case E_NEGATIVE_ARRAY_SIZE              :
         default:
             printf("\n------------------------");              
             printf("\nErro:");                                 
